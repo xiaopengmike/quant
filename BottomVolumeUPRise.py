9f4d48f00f943c8f28ada7e66d83c40684fbe421
Mike <xiaopengmike@163.com>
2023-10-08 22:08:50 +0800

    Update BottomVolumeUPRise.py

--- BottomVolumeUPRise.py
+++ BottomVolumeUPRise.py
@@ -1,7 +1,7 @@
 import json
 #交易日
 date_range = get_all_trade_days()
-day = '2023-09-22'
+day = '2023-09-11'
 preDay = str(date_range[date_range.get_loc(day)-1].date())
 nextDay = str(date_range[date_range.get_loc(day)+1].date())
 next2Day = str(date_range[date_range.get_loc(day)+2].date())
@@ -9,7 +9,7 @@ next3Day = str(date_range[date_range.get_loc(day)+3].date())
 followDayList = [nextDay,next2Day,next3Day]
 
 #查询1
-queryInfo1 = "沪深所有非st股票" + "day + 收盘价 前复权， 和前120日区间最低价 前复权"
+queryInfo1 = "沪深所有非st股票" + "day + 收盘价 前复权， 和前90日区间最低价 前复权"
 df1 = query_iwencai(queryInfo1)
 # df1 = query_iwencai("沪深所有非st股票 20230904 收盘价 前复权， 和前120日区间最低价 前复权")
 
@@ -17,7 +17,7 @@ rowList = []
 for index, row in df1.iterrows():
 #     print(row['收盘价:前复权']) 
 #     print(row['区间最低价:前复权']*1.2)
-    if row['收盘价:前复权'] < row['区间最低价:前复权']*1.2:
+    if row['收盘价:前复权'] < row['区间最低价:前复权']*1.15:
         rowList.append(row)
 #print(rowList)
 len(rowList)
@@ -27,23 +27,35 @@ df1 = pd.DataFrame(rowList)
 #查询2
 queryInfo2 = "沪深所有非st股票" + day + "成交量大于" + preDay + "两倍," + day + "涨幅>5%"
 df2 = query_iwencai(queryInfo2)
+
+# queryInfo2 = "沪深所有非st股票，" + day + "涨幅>5%"
+df2 = query_iwencai(queryInfo2)
 # df2 = query_iwencai("沪深所有非st股票 20230904 成交量大于 20230903 两倍, 20230904 涨幅>5%")
 
-stockList1 = df1['股票代码'].tolist()
+#查询3
+queryInfo3 =  day + " 5日均线斜率>-10," + day + " 10日均线斜率>-10"
+df3 = query_iwencai(queryInfo3)
 
+stockList1 = df1['股票代码'].tolist()
 stockList2 = df2['股票代码'].tolist()
-print(len(stockList2))
+stockList3 = df3['股票代码'].tolist()
 
 print(len(stockList1) )
 print(len(stockList2))
+print(len(stockList3))
 
-intersection_set = set(stockList1).intersection(set(stockList2))
 
+
+set1 = set(stockList1)
+set2 = set(stockList2)
+set3 = set(stockList3)
+
+intersection = set1.intersection(set2, set3)
 # 将交集转换回列表
-intersection_list = list(intersection_set)
-len(intersection_list)
+intersection_list = list(intersection)
 
-intersection_list
+print(intersection_list)
+len(intersection_list)
 
 #后续交易日收盘价
 
@@ -70,19 +82,7 @@ for stock in intersection_list:
         newData.update({tradeDate: zhangDieFu})
     resList.append(newData)
 
-# json_str = resList.dumps
 
-
-
-
-    # print(newData)
-        # dicList.append(itemData)
-    # print(dfList)
-# print(dicList)
-# merged_df = pd.concat(dfList, axis=0)
-
-
-# query_iwencai('600071.SH, 300033.SZ 二级行业板块')
 
 result_string = ','.join(intersection_set)
 result_string
@@ -96,5 +96,49 @@ for res in resList:
     searhRes02 = dfExtend.loc[dfExtend['股票代码'] == res['code'], '所属同花顺二级行业'].iloc[0]
     res['二级行业'] = searhRes02
 
+
+
+#
+sum = 0
+countAbove0 = 0
+for res in resList:
+    percentage_decimal = float(res[nextDay].strip('%'))/100
+    sum = sum + percentage_decimal
+    if percentage_decimal>0:
+        countAbove0 = countAbove0 + 1
+
+average1 = sum/len(resList)
+winRate1 = countAbove0/len(resList)
+
+sum = 0
+countAbove0 = 0
+for res in resList:
+    percentage_decimal = float(res[next2Day].strip('%'))/100
+    sum = sum + percentage_decimal
+    if percentage_decimal>0:
+        countAbove0 = countAbove0 + 1
+
+average2 = sum/len(resList)
+winRate2 = countAbove0/len(resList)
+
+sum = 0
+countAbove0 = 0
+for res in resList:
+    percentage_decimal = float(res[next3Day].strip('%'))/100
+    sum = sum + percentage_decimal
+    if percentage_decimal>0:
+        countAbove0 = countAbove0 + 1
+
+average3 = sum/len(resList)
+winRate3 = countAbove0/len(resList)
+
+newData = {nextDay:average1,next2Day:average2,next3Day:average3}
+newData2 = {nextDay:winRate1,next2Day:winRate2,next3Day:winRate3}
+resList.append(newData)
+resList.append(newData2)
+
 resJson = json.dumps(resList)
-print(resJson)
+# print(resJson)
+print('---------------')
+print(newData)
+print(newData2)
